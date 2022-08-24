538d76eb93586dd60588778f1f209eefe1210f98
beingtechsavy <arpanpadhi2004@gmail.com>
2022-08-24 21:12:11 +0530

    change

---
+++ src/methods/stringex.java
@@ -0,0 +1,12 @@
+package methods;
+
+public class stringex {
+    public static void main(String[] args) {
+        String message=greet();
+        System.out.println(message);
+    }
+    static String greet(){
+        String Greetings="HOW ARE YOU";
+        return Greetings;
+    }
+}
--- src/methods/sum.java
+++ src/methods/sum.java
@@ -4,16 +4,17 @@ import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
-        Sum();
-    }
-    static void Sum(){
         System.out.println("this is a program to add 2 numbers");
         Scanner in=new Scanner(System.in);
         System.out.println("enter the first number");
         int num1=in.nextInt();
         System.out.println("enter the second number");
         int num2=in.nextInt();
-        int sum=num1+num2;
+        Sum(num1,num2);
+    }
+    static void Sum(int a ,int b){
+
+        int sum=a+b;
         System.out.println("the sum of a and b is "+sum);
     }
 }
