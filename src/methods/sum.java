4f9d86028e1ca407b3405b2cc73588afc1e23be7
beingtechsavy <arpanpadhi2004@gmail.com>
2022-08-24 20:07:01 +0530

    slight changes to look better

--- src/methods/sum.java
+++ src/methods/sum.java
@@ -7,8 +7,11 @@ public class sum {
         Sum();
     }
     static void Sum(){
+        System.out.println("this is a program to add 2 numbers");
         Scanner in=new Scanner(System.in);
+        System.out.println("enter the first number");
         int num1=in.nextInt();
+        System.out.println("enter the second number");
         int num2=in.nextInt();
         int sum=num1+num2;
         System.out.println("the sum of a and b is "+sum);
