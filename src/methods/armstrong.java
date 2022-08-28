daf4f68b3fb8791c388cb15fe664a7612cb64589
beingtechsavy <arpanpadhi2004@gmail.com>
2022-08-28 09:49:21 +0530

    armstrong number

--- src/methods/armstrong.java
+++ src/methods/armstrong.java
@@ -6,27 +6,29 @@ public class armstrong {
     public static void main(String[] args) {
         Scanner in =new Scanner(System.in);
         int num=in.nextInt();
-        for (int i=0;i<num;i++){
-            checkarm(i);
-        }
+//        for (int i=0;i<num;i++){
+            checkarm(num);
+//        }
     }
-    static void checkarm(int num){
-        int ans=0;
+    static void checkarm(int num) {
+        int ans = 0;
         int rem;
-        int temp=num;
-        while(num!=0){
-            rem=num%10;
-            ans+=rem*rem*rem;
-            num=num/10;
+        int temp = num;
+        while (num != 0) {
+            rem = num % 10;
+            ans += rem * rem * rem;
+            num = num / 10;
 
         }
+
 //        System.out.println(ans);
-//        System.out.println(temp);
-        if (temp==ans){
-            System.out.println("it is an armstrong number   "+temp);
+    if (temp==ans){
+        System.out.println("it is an armstrong number   "+temp);
+
         }
 //        else {
 //            System.out.println("not an armstrong number");
 //        }
+
     }
 }
