a02e00d53f10230ad2b6d032e0253066a848692f
beingtechsavy <arpanpadhi2004@gmail.com>
2022-09-07 15:27:05 +0530

    this a google interview question

--- src/Binaryinterviewqs/rotatedsearch.java
+++ src/Binaryinterviewqs/rotatedsearch.java
@@ -2,9 +2,10 @@ package Binaryinterviewqs;
 
 public class rotatedsearch {
     public static void main(String[] args) {
-        int[] arr={4,5,6,7,0,1,2};
+        int[] arr={4,5,6,7,8,10,0,1,2,3};
         int target=0;
         System.out.println(pbinarysearch(arr,target));
+        System.out.println(findpeeak(arr));
     }
     static int pbinarysearch(int[] arr,int target){
         int pivot =findpeeak(arr);
@@ -31,11 +32,22 @@ public class rotatedsearch {
             if (arr[mid]<arr[mid-1] && mid>start){
                 return mid-1;
             }
-            if (arr[mid]<=arr[start]){
-                end=mid-1;
+            if(arr[mid]==arr[start] && arr[mid]==arr[end]){
+                if (arr[start]>arr[start+1]){
+                    return start;
+                }
+                if (arr[end]<arr[end-1]){
+                    return end-1;
+                }
+                start++;
+
+                end--;
+            } else if (arr[start]<arr[mid] || arr[start]==arr[mid] && arr[mid]>arr[end]) {
+                start=mid+1;
+                
             }
             else {
-                start=mid+1;
+                end=mid-1;
             }
         }
         return -1;
