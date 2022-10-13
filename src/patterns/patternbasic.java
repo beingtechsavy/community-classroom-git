c1b919c213136a879228af0894499f3b4a7a2d2b
beingtechsavy <arpanpadhi2004@gmail.com>
2022-10-13 21:38:52 +0530

    pattern exercises

--- src/patterns/patternbasic.java
+++ src/patterns/patternbasic.java
@@ -2,7 +2,8 @@ package patterns;
 import java.lang.Math;
 public class patternbasic {
     public static void main(String[] argcs){
-        pattern31(9);
+        patterna10(5);
+//        patterna2(10);
 //        pattern1(10);
 //        pattern2(10);
 //        pattern3(10);
@@ -109,6 +110,146 @@ public class patternbasic {
             System.out.println();
         }
     }
+    static void patterna1(int n){
+        for (int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for (int k=0;k<n-i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void patterna2(int n){
+        for (int i=0;i<n;i++){
+            for (int j=n;j>i;j--){
+                System.out.print(" ");
+            }
+            for (int k=0;k<2*i+1;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void patterna3(int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for (int k=2*n-1;k>2*i;k--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    static void patterna4(int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for (int k=0;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void patterna5(int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<i;j++){
+                System.out.print(" ");
+            }
+            for (int k=n;k>i;k--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void patterna6(int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<=i;j++){
+                System.out.print(" ");
+            }
+            for (int k=n;k>i;k--){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n-i;j++){
+                System.out.print(" ");
+            }
+            for (int k=0;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    static void patterna7(int n){
+        for (int i=0;i<n-1;i++){
 
+            for (int k=1;k<2*n;k++){
+                if(k==(n-i)||k==(n+i)){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        for(int j=0;j<2*n-1;j++){
+            System.out.print("*");
+        }
+    }
+    static void patterna8(int n){
+        for(int j=0;j<2*n-1;j++){
+            System.out.print("*");
+
+        }
+        System.out.println();
+        for (int i=1;i<n;i++){
+
+            for (int k=1;k<2*n;k++){
+
+                if(k==(i+1)||k==(2*n-i-1)){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    static void patterna9(int n){
+        for (int i=1;i<2*n;i++){
+            for( int j=1;j<2*n;j++){
+                int index=i>n?2*n-i:i;
+                if (j==(n-index+1)||j==(n+index-1)){
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+    }
+    static void patterna10(int n){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<2*n-2*i;j++){
+                System.out.print(" ");
+            }
+            for (int k=1;k<=i+1;k++){
+                if (k>1 || k<i+1){
+
+                }
+            }
+            System.out.println();
+        }
+    }
 
 }
